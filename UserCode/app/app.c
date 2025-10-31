dfb06a0ed1b0b3e9286166ef64c01fd7d6489a56
syhanjin <syhanjin666@gmail.com>
2025-10-31 23:17:35 +0800

    test: add init example

--- UserCode/app/app.c
+++ UserCode/app/app.c
@@ -5,6 +5,19 @@
  */
 #include "app.h"
 #include "cmsis_os2.h"
+#include "usart.h"
+
+#include "drivers/stp32l.h"
+
+STP32L_t stp32l;
+
+void STP32L_Uart_RxCpltCallback(UART_HandleTypeDef* huart)
+{
+    if (huart == stp32l.huart)
+    {
+        STP32L_RxCallback(&stp32l);
+    }
+}
 
 /**
  * @brief Function implementing the initTask thread.
@@ -13,7 +26,9 @@
  */
 void Init(void* argument)
 {
-	/* 初始化代码 */
+    /* 初始化代码 */
+    HAL_UART_RegisterCallback(stp32l.huart, HAL_UART_RX_COMPLETE_CB_ID, STP32L_Uart_RxCpltCallback);
+    STP32L_Init(&stp32l, &huart4);
 
     /* 初始化完成后退出线程 */
     osThreadExit();
